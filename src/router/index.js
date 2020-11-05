f771d218f69824ffad8f745f0341b7458b13633f
Damian Sierocki <damiansierocki@outlook.com>
2020-11-05 11:49:01 +0100

    add view register and logic with firebase

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 // import Home from "../views/Home.vue";
+import Register from "../views/Register.vue";
 
 Vue.use(VueRouter);
 
@@ -10,6 +11,11 @@ const routes = [
     //     name: "Dashboard",
     //     component: Dashboard,
     // },
+    {
+        path: "/register",
+        name: "Register",
+        component: Register
+    }
 ];
 
 const router = new VueRouter({
