4631a8861c7d054ad57ee9276fd5af78b1debf7c
damiansierocki <damiansierocki@outlook.com>
2020-12-02 22:31:13 +0100

    fixed ViewApiary

--- src/components/ViewHives.vue
+++ src/components/ViewHives.vue
@@ -1,129 +1,95 @@
 <template>
-    <div class="hivesview">
-        <div class="content">
-            <div class="content__header">
-                <h2 class="content__title">
-                    Opis ula 🐝
-                </h2>
-                <span class="content__close" @click="$emit('close')">
+    <div class="viewhives">
+        <div class="container">
+            <div class="container__inside">
+                <div class="container__close" @click="$emit('close')">
                     <i class="fas fa-times"></i>
-                </span>
-            </div>
-
-            <div class="info">
-                <div class="info__general">
-                    <h3 class="info__title-h3">Informacje generalne</h3>
-
-                    <p class="info__p info__p--title">Numer Ula</p>
-                    <p class="info__p info__p--description">
-                        {{ number }}
-                    </p>
-
-                    <p class="info__p info__p--title">Pasieka</p>
-                    <p class="info__p info__p--description">
-                        {{ apiary }}
-                    </p>
-
-                    <p class="info__p info__p--title">Stan ula</p>
-                    <p class="info__p info__p--description">
-                        {{ status }}
-                    </p>
-
-                    <p class="info__p info__p--title">Przeznaczenie</p>
-                    <p class="info__p info__p--description">
-                        {{ purpose }}
-                    </p>
-
-                    <p class="info__p info__p--title">
-                        Data dodania ula do pasieki
-                    </p>
-                    <p class="info__p info__p--description">
-                        {{ created }}
-                    </p>
-
-                    <p class="info__p info__p--title">Notatka odnośnie ula</p>
-                    <p class="info__p info__p--description">
-                        {{ generalNote }}
-                    </p>
                 </div>
 
-                <div class="info__bees">
-                    <h3 class="info__title-h3">Pszczoły</h3>
+                <h2 class="container__header">Twój ul</h2>
 
-                    <p class="info__p info__p--title">Siła pszczół</p>
-                    <p class="info__p info__p--description">
-                        {{ strength }}
-                    </p>
+                <h2 class="container__header container__header--desc">
+                    Informacje generalne
+                </h2>
+                <p class="container__label">Numer ula</p>
+                <p class="container__text">
+                    {{ number }}
+                </p>
 
-                    <p class="info__p info__p--title">Temperament</p>
-                    <p class="info__p info__p--description">
-                        {{ temperament }}
-                    </p>
+                <p class="container__label">Pasieka</p>
+                <p class="container__text">
+                    {{ apiary }}
+                </p>
 
-                    <p class="info__p info__p--title">Ilość ramek w ulu</p>
-                    <p class="info__p info__p--description">
-                        {{ frames }}
-                    </p>
-                </div>
+                <p class="container__label">Stan ula</p>
+                <p class="container__text">
+                    {{ status }}
+                </p>
 
-                <div class="info__queen">
-                    <h3 class="info__title-h3">Królowa</h3>
+                <p class="container__label">Przeznaczenie</p>
+                <p class="container__text">
+                    {{ purpose }}
+                </p>
 
-                    <p class="info__p info__p--title">Królowa</p>
-                    <p class="info__p info__p--description">
-                        {{ isQueen }}
-                    </p>
+                <p class="container__label">Data dodania ula do pasieki</p>
+                <p class="container__text">
+                    {{ created }}
+                </p>
 
-                    <p class="info__p info__p--title" v-if="isQueen === 'Tak'">
-                        Wiek Królowej (w latach)
-                    </p>
-                    <p
-                        class="info__p info__p--description"
-                        v-if="isQueen === 'Tak'"
-                    >
-                        {{ age }}
-                    </p>
+                <h2 class="container__header container__header--desc">
+                    Informacje o pszczołach
+                </h2>
 
-                    <p class="info__p info__p--title" v-if="isQueen === 'Tak'">
-                        Kiedy królowa została wrzucona do ula
-                    </p>
-                    <p
-                        class="info__p info__p--description"
-                        v-if="isQueen === 'Tak'"
-                    >
-                        {{ installed }}
-                    </p>
+                <p class="container__label">Siła pszczół</p>
+                <p class="container__text">
+                    {{ strength }}
+                </p>
 
-                    <p class="info__p info__p--title" v-if="isQueen === 'Tak'">
-                        Rasa królowej
-                    </p>
-                    <p
-                        class="info__p info__p--description"
-                        v-if="isQueen === 'Tak'"
-                    >
-                        {{ race }}
-                    </p>
+                <p class="container__label">Temperament</p>
+                <p class="container__text">
+                    {{ temperament }}
+                </p>
 
-                    <p class="info__p info__p--title" v-if="isQueen === 'Tak'">
-                        Kolor królowej
-                    </p>
-                    <p
-                        class="info__p info__p--description"
-                        v-if="isQueen === 'Tak'"
-                    >
-                        {{ queenColor }}
-                    </p>
+                <p class="container__label">Ilość ramek w ulu</p>
+                <p class="container__text">
+                    {{ frames }}
+                </p>
 
-                    <p class="info__p info__p--title" v-if="isQueen === 'Tak'">
-                        Notatka dotycząca królowej
-                    </p>
-                    <p
-                        class="info__p info__p--description"
-                        v-if="isQueen === 'Tak'"
-                    >
-                        {{ queenNote }}
-                    </p>
-                </div>
+                <h2 class="form__header form__header--desc">
+                    Informacje o matce
+                </h2>
+
+                <p class="container__label">Matka</p>
+                <p class="container__text">
+                    {{ isQueen }}
+                </p>
+
+                <p class="container__label">Wiek Matki (w latach)</p>
+                <p class="container__text">
+                    {{ age }}
+                </p>
+
+                <p class="container__label">
+                    Kiedy matka została wrzucona do ula
+                </p>
+                <p class="container__text">
+                    {{ installed }}
+                </p>
+
+                <p class="container__label">Rasa matki</p>
+                <p class="container__text">
+                    {{ race }}
+                </p>
+
+                <p class="container__label">Kolor matki</p>
+                <p class="container__text">
+                    {{ queenColor }}
+                </p>
+
+                <p class="container__label">Notatka dotycząca matki</p>
+                <p class="container__text">
+                    {{ queenNote }}
+                </p>
             </div>
         </div>
     </div>
@@ -153,135 +119,88 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '../assets/scss/colors';
-
-.hivesview {
+.viewhives {
     position: absolute;
-    top: 0;
-    right: 0;
-    left: 0;
-    bottom: 0;
-    z-index: 999;
+    z-index: 1;
     display: flex;
+    width: 100%;
 
-    .content {
+    .container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
         width: 100%;
-        background: $white;
+        background: white;
+        position: relative;
 
-        &__header {
+        &__inside {
+            width: 100%;
             display: flex;
             flex-direction: column;
-            align-items: center;
-            justify-items: center;
-            margin-top: 3rem;
-        }
-
-        &__title {
-            font-size: 2rem;
+            justify-content: center;
+            border-radius: 0.2rem;
+            padding: 3rem;
+            position: relative;
         }
 
         &__close {
             position: absolute;
-            top: 1%;
-            right: 5%;
-            padding: 0.5rem;
-            font-size: 1.2rem;
+            padding: 1rem 2rem;
+            font-size: 2rem;
+            top: 0;
+            right: 0;
+            cursor: pointer;
         }
 
-        .info {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-items: center;
+        &__header {
+            font-size: 2rem;
+            position: relative;
+            padding: 2rem 0rem;
+
+            &--desc {
+                font-size: 1.5rem;
+
+                &::after {
+                    display: none;
+                }
+            }
+
+            &::after {
+                content: '';
+                position: absolute;
+                left: 0;
+                right: 0;
+                bottom: 0;
+                width: 100%;
+                height: 0.5px;
+                background: #000000;
+            }
+        }
+
+        &__label {
+            margin: 2rem 0;
+            font-size: 1.5rem;
+        }
+
+        &__text {
+            border: 1px solid #ccc;
+            padding: 1rem;
+            border-radius: 0.2rem;
+            font-size: 1.2rem;
+            position: relative;
+        }
+    }
+}
+
+@media (min-width: 768px) {
+    .container {
+        background-color: white;
+
+        &__inside {
             margin-top: 3rem;
-            background: $base-white;
-
-            &__center {
-                display: flex;
-                flex-direction: column;
-                margin-top: 2rem;
-                justify-content: center;
-                align-items: center;
-            }
-
-            &__general {
-                display: flex;
-                flex-direction: column;
-                width: 100%;
-                justify-content: center;
-                align-items: center;
-                border: 2px solid $black;
-                padding: 1rem;
-            }
-
-            &__bees {
-                display: flex;
-                flex-direction: column;
-                width: 100%;
-                justify-content: center;
-                align-items: center;
-                border: 2px solid $black;
-                padding: 1rem;
-            }
-
-            &__queen {
-                display: flex;
-                flex-direction: column;
-                width: 100%;
-                justify-content: center;
-                align-items: center;
-                border: 2px solid $black;
-                padding: 1rem;
-            }
-
-            &__title-h3 {
-                font-size: 1.8rem;
-                font-weight: bold;
-                border-bottom: 5px solid $black;
-            }
-
-            &__status {
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-            }
-
-            &__hive-id {
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-            }
-
-            &__apiary {
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-            }
-
-            &__queen-color {
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-            }
-
-            &__p {
-                margin-top: 0.4rem;
-
-                &--title {
-                    font-size: 1.5rem;
-                    font-weight: bold;
-                    color: $orange;
-                    padding: 0.7rem;
-                }
-
-                &--description {
-                    font-size: 1rem;
-                }
-            }
+            border: 1px solid #ccc;
+            max-width: 768px;
         }
     }
 }
