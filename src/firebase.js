ff465d9b8e835c3adf846ca9b3b693cdbe039d3b
damiansierocki <damiansierocki@outlook.com>
2020-12-05 16:46:26 +0100

    Delete data in firebase.js.

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/firestore';
 
 // firebase init - add your own config here
 const firebaseConfig = {
-    apiKey: 'AIzaSyAsGTRShu3VWiBtM8aSR7quCqff3Y3ay9E',
-    authDomain: 'beeapp-62f71.firebaseapp.com',
-    projectId: 'beeapp-62f71',
-    storageBucket: 'beeapp-62f71.appspot.com',
-    messagingSenderId: '956205510168',
-    appId: '1:956205510168:web:ecfa09d1fa4ced6d27dd6d',
+    apiKey: '*',
+    authDomain: '*',
+    projectId: '*',
+    storageBucket: '*',
+    messagingSenderId: '*',
+    appId: '*',
 };
 firebase.initializeApp(firebaseConfig);
 
